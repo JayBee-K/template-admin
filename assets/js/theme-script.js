1e1c05566754a1ecb486802bd1d62ecbba07f6bc
Jay B <killerdrug007@gmail.com>
2021-04-06 14:22:31 +0700

    Push

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -1,28 +1,113 @@
 $(document).ready(function () {
-// Active sidebar dashboard
-// 	var activeSidebar = function () {
-// 		$('.template-administrator .sidebar .navbar .collapse').on('show.bs.collapse', function () {
-// 			$(this).prev('.nav-link').addClass('active');
-// 			$('.template-administrator .sidebar .navbar > .nav > .nav-item > .nav-link').removeClass('active');
-// 		})
-// 	};
-// 	activeSidebar();
-
-//  Toggle Sidebar - Toggle Class element Boby
-// let windowWidth = $(window).width();
-	var toggleSidebar = function () {
-		$('.template-administrator .toggleSidebar').click(function () {
-			$('.template-administrator').toggleClass('active-sidebar');
-			// if (windowWidth > 992) {
-			// $('.sidebar .navbar .collapse.show').removeClass('show');
-			// }
+	var call_toastr = function (msg, type, time, position, parentElm) {
+		if (!position) {
+			position = "position-fixed bottom-0 end-0";
+		}
+		let classFind = position.replaceAll(" ", ".");
+		if (!time) {
+			time = 3000;
+		}
+		let html = "";
+		let hasContainer = ($(".toast-container." + classFind).length > 0);
+		let icon = "";
+		let bg = "";
+		let textColor = "";
+		if (type == "success") {
+			icon = '<i class="bi bi-check-circle" style="vertical-align: .125em"></i>';
+			bg = "bg-success bg-gradient";
+			textColor = "text-white";
+		} else if (type == "info") {
+			icon = '<i class="bi bi-info-circle" style="vertical-align: .125em"></i>';
+			bg = "bg-info bg-gradient";
+			textColor = "text-white";
+		} else if (type == "warning") {
+			icon = '<i class="bi bi-exclamation-triangle" style="vertical-align: .125em"></i>';
+			bg = "bg-warning bg-gradient";
+			textColor = "text-white";
+		} else if (type == "error") {
+			icon = '<i class="bi bi-x-circle" style="vertical-align: .125em"></i>';
+			bg = "bg-danger bg-gradient";
+			textColor = "text-white";
+		}
+		if (!hasContainer) {
+			html += `<div class="toast-container ${position}">`;
+		}
+		html += `<div class="toast ${bg} ${textColor}" role="alert" aria-live="assertive" data-delay="${time}"
+         aria-atomic="true">
+		<div class="toast-body d-flex">
+			 <div class="me-2 ml-auto">
+				${icon}
+			 </div>
+			  ${msg}
+			<button type="button" class="btn-close btn-close-white ms-2 mr-auto" data-dismiss="toast" aria-label="Close"></button>
+		</div>
+    </div>`;
+		
+		let elmContainer;
+		if (!hasContainer) {
+			html += `</div>`;
+			if (parentElm) {
+				parentElm.append(html);
+			} else {
+				$("body").append(html);
+			}
+			elmContainer = $(".toast-container." + classFind);
+		} else {
+			elmContainer = $(".toast-container." + classFind);
+			elmContainer.append(html);
+		}
+		
+		elmContainer.find(".toast:not(.fade)")[0].addEventListener('hidden.bs.toast', function (e) {
+			e.target.remove();
+			if (elmContainer.find(".toast").length == 0) {
+				elmContainer.remove();
+			}
 		});
+		
+		elmContainer.find(".toast:not(.fade)").toast("show");
+	};
+	
+	function callSearch(el) {
+		if (el.closest('#mainSearch').hasClass('active')) {
+			el.closest('#mainSearch').removeClass('active');
+		} else {
+			el.closest('#mainSearch').addClass('active');
+		}
 	}
 	
+	var toggleSidebar = function () {
+		$('.template-administrator .toggleSidebar, .sidebar-overlay').click(function () {
+			$('.template-administrator').toggleClass('active-sidebar');
+		});
+	}
 	toggleSidebar();
 	
+	var checkAll = function () {
+		$(document).on("click", "#checkall", function () {
+			if ($(".mycheckbox").is(":checked")) {
+				$('.mycheckbox').prop('checked', false);
+			} else {
+				$('.mycheckbox').prop('checked', true);
+			}
+		});
+	}
+	checkAll();
+	
+	
+	let windowWidth = $(window).width();
+	if (windowWidth < 992) {
+		if ($('.template-administrator').hasClass('active-sidebar')) {
+			$('.template-administrator').removeClass('active-sidebar')
+		}
+	}
 	$('.template-administrator .sidebar-overlay').click(function (e) {
 		$('.home-page').removeClass('active-sidebar');
-	})
+	});
 	
+	$('#mainSearch button.search-icon, #mainSearch button.close').click(function (e) {
+		if (!$(this).closest('#mainSearch').hasClass('active')) {
+			e.preventDefault();
+		}
+		callSearch($(this));
+	});
 })
